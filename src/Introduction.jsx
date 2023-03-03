4019c87b91ffd2c1495e579efc3474efa32b6869
jakohole <jakohole@users.noreply.github.com>
2023-03-03 10:38:53 +0700

    2 files modified, 4 added

---
+++ src/Introduction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Introduction() {
+  return (
+    <div id="introduction">
+      <h2>Introduction</h2>
+      <p>Welcome to the KEEN Organizational Ethics Assessment!</p>
+    </div>
+  );
+}
+
+export default Introduction;
