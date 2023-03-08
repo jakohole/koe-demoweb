0bff928f93286ef6afd3829339f82e1d54823596
jakohole <jakohole@users.noreply.github.com>
2023-03-08 10:23:18 +0700

    3 files modified

--- src/components/SpeedQuiz.jsx
+++ src/components/SpeedQuiz.jsx
@@ -10,28 +10,16 @@ function SpeedQuiz() {
   const [showNextButton, setShowNextButton] = useState(false);
   const [countdownStarted, setCountdownStarted] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(0);
-  /*const questions = [
-    {
-      question: "What is your favorite color?",
-      choices: ["Red", "Blue", "Green", "Yellow", "Purple"],
-      answer: 1 // index of the correct answer in the choices array
-    },
-    {
-      question: "What is your favorite animal?",
-      choices: ["Dog", "Cat", "Bird", "Fish", "Other"],
-      answer: 0
-    },
-    // Add more questions here
-  ];*/
 
   const handleShowChoices = () => {
     setShowChoices(true);
-    setShowNextButton(true);
+    setShowNextButton(false);
+    setSelectedChoice(null);
     setCountdownStarted(true);
   };
 
   const handleSelectChoice = (event) => {
-    setSelectedChoice(event.target.value);
+    setSelectedChoice(parseInt(event.target.value));
     setShowNextButton(true);
   };
 
@@ -42,53 +30,65 @@ function SpeedQuiz() {
       currentQuestion.choices[selectedChoice] ===
       currentQuestion.choices[currentQuestion.answer];
 
-    // Move to the next question
-    setQuestionNumber(questionNumber + 1);
-    setSelectedChoice(null);
-    setShowNextButton(false);
-    setShowChoices(false);
-    setCountdownStarted(false);
-
-    // Display a message based on the selected choice
-    if (isCorrect) {
-      alert('Correct!');
+    // Move to the next question or end of the quiz
+    if (questionNumber === questions.length - 1) {
+      //alert('Quiz is over!'); // display a message to show the end of the quiz}
+      setQuestionNumber(0); // restart the quiz
+      setShowChoices(false);
+      setShowNextButton(false);
+      setSelectedChoice(null);
+      setCountdownStarted(false);
     } else {
-      alert('Incorrect!');
+      setQuestionNumber(questionNumber + 1);
+      setShowChoices(false);
+      setShowNextButton(false);
+      setSelectedChoice(null);
+      setCountdownStarted(false);
+
+      // Display a message based on the selected choice
+      /*
+      if (isCorrect) {
+        alert('Correct!');
+      } else {
+        alert('Incorrect!');
+      }
+      */
     }
   };
 
+  const [countdown, setCountdown] = useState(10);
   useEffect(() => {
-    // Move to the next question when the timer runs out
-    if (countdownStarted && countdown === 0) {
+    let intervalId;
+    if (countdownStarted && countdown > 0) {
+      intervalId = setInterval(() => setCountdown(countdown - 1), 2000);
+    } else if (countdownStarted && countdown === 0) {
+      clearInterval(intervalId);
       handleNext();
     }
-  }, [countdownStarted, countdown]);
+    return () => clearInterval(intervalId);
+  }, [countdown, countdownStarted, handleNext]);
 
   const currentQuestion = questions[questionNumber];
 
   return (
     <div className="box" id="speedquiz">
       <p>{currentQuestion.question}</p>
-      {showChoices ? (
+      {currentQuestion && showChoices ? (
         <div>
-          <Timer countdownStarted={countdownStarted} />
+          <Timer countdown={countdown} />
           {currentQuestion.choices.map((choice, index) => (
             <div key={index}>
               <input
                 type="radio"
                 name="choice"
                 value={index}
-                checked={selectedChoice == index}
+                checked={selectedChoice === index}
                 onChange={handleSelectChoice}
               />
               <label htmlFor="choice">{choice}</label>
             </div>
           ))}
-          {showNextButton && (
-            <button onClick={handleNext}>
-              {questionNumber === questions.length - 1 ? 'Next Part' : 'Next'}
-            </button>
-          )}
+          {showNextButton && <button onClick={handleNext}>Next</button>}
         </div>
       ) : (
         <button onClick={handleShowChoices}>Show choices</button>
--- src/model/questiondata.jsx
+++ src/model/questiondata.jsx
@@ -1,17 +1,17 @@
 const QuestionData = [
   {
     question: 'What is your favorite color?',
-    options: ['Red', 'Blue', 'Green', 'Yellow', 'Purple'],
+    choices: ['Red', 'Blue', 'Green', 'Yellow', 'Purple'],
     answer: 'Blue',
   },
   {
     question: 'What is the capital of France?',
-    options: ['Madrid', 'Berlin', 'Paris', 'Rome', 'Vienna'],
+    choices: ['Madrid', 'Berlin', 'Paris', 'Rome', 'Vienna'],
     answer: 'Paris',
   },
   {
     question: 'What is 2 + 2?',
-    options: ['1', '2', '3', '4', '5'],
+    choices: ['1', '2', '3', '4', '5'],
     answer: '4',
   },
 ];
