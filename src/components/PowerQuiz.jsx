f8ce4eb2f9d1c04f71fe404a252d629cbb352135
jakohole <jakohole@users.noreply.github.com>
2023-03-17 12:56:29 +0700

    2 files modified

--- src/components/PowerQuiz.jsx
+++ src/components/PowerQuiz.jsx
@@ -51,7 +51,7 @@ function PowerQuiz() {
             ref={(el) => (questionRefs.current[index] = el)}
             className="bg-transparent rounded-md shadow-lg p-4 mb-4"
           >
-            <p className="text-lg font-bold text-left">{question.question}</p>
+            <p className="text-lg text-left">{question.question}</p>
             {question.choices.map((choice, choiceIndex) => (
               <div key={choiceIndex} className="my-4 flex text-left">
                 {
