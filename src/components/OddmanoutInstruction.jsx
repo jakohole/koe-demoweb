84272e4c13fcb4c4db45c9f2692a19c71b95d039
jakohole <jakohole@users.noreply.github.com>
2023-03-17 12:57:25 +0700

    1 file modified

--- src/components/OddmanoutInstruction.jsx
+++ src/components/OddmanoutInstruction.jsx
@@ -7,17 +7,27 @@ function OddmanoutInstruction() {
       <div className="flex flex-col items-start justify-end bg-white shadow rounded-lg">
         <p className="px-4 py-4 text-lg">คำอธิบาย</p>
         <ul className="text-left px-8 list-disc">
-          <li className="py-2">คุณมีเวลา <span className="font-semibold">ไม่จำกัด</span> ในการตอบคำถามทุกข้อ</li>
           <li className="py-2">
-            คุณสามารถเลือกคำตอบที่ <span className="font-semibold">zbf </span>ได้หลายคำตอบ{' '}
-          </li>
-          <li className="py-2">คุณสามารถ <span className="font-semibold">ย้อนกลับ</span>ไปที่คำถามก่อนหน้าได้</li>
-          
-          <li className="py-2">
-            เมื่อทำข้อสอบเสร็จแล้วกดปุ่ม <span className="font-semibold">ส่งคำตอบ </span>เพื่อไปสู่ข้อคำถามพาร์ทถัดไป
+            คุณมีเวลา <span className="font-semibold">ไม่จำกัด</span>{' '}
+            ในการตอบคำถามทุกข้อ
           </li>
           <li className="py-2">
-            กดปุ่ม <span className="font-semibold">เริ่มทำแบบประเมิน </span> เพื่อเริ่มการทำแบบประเมินในพาร์ทที่ 1.2
+            คุณสามารถเลือกคำตอบที่ <span className="font-semibold">ผิด </span>
+            ได้หลายคำตอบ{' '}
+          </li>
+          <li className="py-2">
+            คุณสามารถ <span className="font-semibold">ย้อนกลับ</span>
+            ไปที่คำถามก่อนหน้าได้
+          </li>
+
+          <li className="py-2">
+            เมื่อทำข้อสอบเสร็จแล้วกดปุ่ม{' '}
+            <span className="font-semibold">ส่งคำตอบ </span>
+            เพื่อไปสู่ข้อคำถามพาร์ทถัดไป
+          </li>
+          <li className="py-2">
+            กดปุ่ม <span className="font-semibold">เริ่มทำแบบประเมิน </span>{' '}
+            เพื่อเริ่มการทำแบบประเมินในพาร์ทที่ 1.2
           </li>
         </ul>
       </div>
