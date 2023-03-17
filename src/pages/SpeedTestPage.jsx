eadcd84521f0475c8d696ecbee31a78e6343820b
jakohole <jakohole@users.noreply.github.com>
2023-03-17 09:48:25 +0700

    1 file modified

--- src/pages/SpeedTestPage.jsx
+++ src/pages/SpeedTestPage.jsx
@@ -7,9 +7,11 @@ import '../styles/Quiz.css';
 
 function SpeedTestPage() {
   const [showQuiz, setShowQuiz] = useState(false);
+  const [showTitle, setShowTitle] = useState(true);
 
   const handleStartQuiz = () => {
     setShowQuiz(true);
+    setShowTitle(false);
   };
 
   // Add event listener to enable scrolling again when card component is no longer fixed
@@ -23,27 +25,26 @@ function SpeedTestPage() {
   }, []);
 
   return (
-    <div >
+    <div>
       <div className="flex flex-col items-center justify-center">
-        <div className="inline-flex items-start justify-center px-24 pt-26 mt-10 mb-4">
-          <p className="font-gothic text-4xl font-bold text-white">
-            Part 1-1 - Moral Temptation : Speed Test
-          </p>
-        </div>
-        <div className="py-4">
-          {/*<h2 style={{ position: 'absolute', top: 0, left: 0, right: 0 }}>
-        Part 1.1 Moral Temptations (right vs wrong) - Speed Test
-      </h2>*/}
-          {showQuiz ? <SpeedQuiz /> : <SpeedTest />}
-          {showQuiz ? null : (
+        {showTitle && (
+          <div className="inline-flex items-start justify-center px-24 pt-26 mt-10 mb-4">
+            <p className="font-gothic text-4xl font-bold text-white">
+              Part 1-1 - Moral Temptation : Speed Test
+            </p>
+          </div>
+        )}
+        {showQuiz ? <SpeedQuiz /> : <SpeedTest />}
+        {showQuiz ? null : (
+          <div className="py-4">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded-full"
               onClick={handleStartQuiz}
             >
               เริ่มทำแบบประเมิน
             </button>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
