e92ec3ae1337b0cb67ad8c7657966a83c763e990
jakohole <jakohole@users.noreply.github.com>
2023-03-22 11:51:45 +0700

    2 files modified

--- src/components/ScalesQuiz.jsx
+++ src/components/ScalesQuiz.jsx
@@ -90,9 +90,11 @@ function ScalesQuiz() {
   return (
     <div>
       <div className="container items-left px-24 py-10">
-        <p className="text-lg text-left">{currentQuestion.question}</p>
+        <p className="text-base text-left text-white">
+          {currentQuestion.question}
+        </p>
         {currentQuestion && showChoices ? (
-          <div className="mt-2 text-lg">
+          <div className="mt-2 text-base ">
             {<Timer countdown={countdown} />}
             {showChoices && (
               <TenScalesChoices
--- src/components/TenScalesChoices.jsx
+++ src/components/TenScalesChoices.jsx
@@ -9,62 +9,62 @@ function TenScalesChoices() {
 
   return (
     <div
-      className="flex space-x-2 items-start justify-center py-4 "
+      className="flex space-x-2 items-start justify-center py-5 "
       style={{ width: 800, height: 120 }}
     >
       {[...Array(5)].map((_, index) => (
         <div
           key={index}
-          className={`w-10/5 h-full ${
+          className={` w-full h-full ${
             index === selected
               ? 'bg-gradient-to-t from-teal-300 to-teal-500'
               : 'bg-white'
           } hover:text-white hover:bg-gray-200`}
           onClick={() => handleSelect(index)}
         >
-          <div className="flex items-center justify-center flex-1 h-full px-9 pt-6 pb-8">
+          <div className="flex items-center justify-center flex-1  h-full px-6 pt-6 pb-8">
             <p
-              className={`flex-1 h-full text-xl font-lg leading-loose text-center font-semibold${
+              className={`flex-1 h-full text-base leading-loose text-center font-semibold${
                 selected === index ? 'text-white' : ''
               } hover:text-white`}
             >
               {5 - index}
             </p>
           </div>
-          {index === 0 && (
+          {/*index === 0 && (
             <p className="text-left text-gray-500 text-sm">
               {'การปรับตัว (Adaptation)'}
             </p>
-          )}
+          )*/}
         </div>
       ))}
-      <div className="w-10/5 h-full">
-        <div className="flex items-center justify-center flex-1 h-full px-4 pt-6 pb-8"></div>
+      <div className="h-full">
+        <div className="flex items-center justify-center flex-1 w-3 h-full  pt-6 pb-8"></div>
       </div>
       {[...Array(5)].map((_, index) => (
         <div
           key={index}
-          className={`w-10/5 h-full ${
+          className={`w-full h-full ${
             index + 5 === selected
               ? 'bg-gradient-to-t from-teal-300 to-teal-500'
               : 'bg-white'
           } hover:text-white hover:bg-gray-200`}
           onClick={() => handleSelect(index + 5)}
         >
-          <div className="flex items-center justify-center flex-1 h-full px-9 pt-6 pb-8">
+          <div className="flex items-center justify-center flex-1 w-3 h-full px-9 pt-6 pb-8">
             <p
-              className={`flex-1 h-full text-xl font-lg leading-loose text-center font-semibold${
+              className={`flex-1 h-full text-base leading-loose text-center font-semibold${
                 selected === index + 5 ? 'text-white' : ''
               } hover:text-white`}
             >
               {index + 1}
             </p>
           </div>
-          {index === 4 && (
+          {/*index === 4 && (
             <p className="text-right1 text-gray-500 text-sm">
               {'ความเข้มงวด (Rigour)'}
             </p>
-          )}
+          )*/}
         </div>
       ))}
     </div>
