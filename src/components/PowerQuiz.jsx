0d1b0b9ebee82d20d29a633d922c88cd93a3fd26
jakohole <jakohole@users.noreply.github.com>
2023-03-23 13:56:26 +0700

    1 file modified

--- src/components/PowerQuiz.jsx
+++ src/components/PowerQuiz.jsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+} from 'react';
 import questions from '../model/questiondata';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,14 +12,42 @@ function PowerQuiz() {
   //Create navigate variable
   const navigate = useNavigate();
   //Create selectedChoices variable
-  const [selectedChoices, setSelectedChoices] = useState(Array(3).fill(null));
-  //Create currentQuestion variable
-  const [currentQuestion, setCurrentQuestion] = useState(
-    questions.findIndex((q) => q.type === 3)
+  const [selectedChoices, setSelectedChoices] = useState(
+    Array.from({ length: questions.length }, () => [])
   );
+  //Create currentQuestion variable
+  const currentQuestionIndex = useMemo(
+    () => questions.findIndex((q) => q.type === 2),
+    [questions]
+  );
+  const [activeQuestionIndex, setActiveQuestionIndex] =
+    useState(currentQuestionIndex);
 
   //Create questionRefs variable
   const questionRefs = useRef([]);
+  const handleScroll = useCallback(() => {
+    const newActiveQuestionIndex = questionRefs.current.findIndex((el) => {
+      const { top, bottom } = el.getBoundingClientRect();
+      const windowHeight =
+        window.innerHeight || document.documentElement.clientHeight;
+      return top <= windowHeight / 2 && bottom >= windowHeight / 2;
+    });
+
+    if (
+      newActiveQuestionIndex >= 0 &&
+      newActiveQuestionIndex !== activeQuestionIndex
+    ) {
+      setActiveQuestionIndex(newActiveQuestionIndex);
+    }
+  }, [activeQuestionIndex]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
   //Create handleSelectChoice function
   const handleSelectChoice = (event) => {
     const questionIndex = parseInt(event.target.name.split('-')[1]);
@@ -23,39 +57,42 @@ function PowerQuiz() {
         index === questionIndex ? choiceIndex : choices
       )
     );
+
+    // Scroll to next question
+    const nextQuestionIndex = questionIndex + 1;
+    if (nextQuestionIndex < questions.length) {
+      questionRefs.current[nextQuestionIndex].scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
   };
+
   const handleSubmit = () => {
     navigate('/scalestest');
   };
-  //Create handleNextQuestion function
-  /*
-  const handleNextQuestion = () => {
-    if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1);
-      questionRefs.current[currentQuestion + 1].scrollIntoView({
-        behavior: 'smooth',
-      });
-    } else {
-      navigate('/scalestest');
-    }
-  };
-*/
-  //Return OddmanoutQuiz component
+
   return (
-    <div className="container px-8 py-8">
+    <div className="container px-6 py-8">
       {questions
         .filter((question) => question.type === 3)
         .map((question, index) => (
           <div
             key={index}
             ref={(el) => (questionRefs.current[index] = el)}
-            className="bg-transparent rounded-md shadow-lg p-4 mb-4 text-base text-white"
+            className={`flex-col mb-20 ${
+              index === activeQuestionIndex
+                ? ''
+                : 'opacity-50 pointer-events-none'
+            }`}
           >
-            <p className="text-base text-left">{question.question}</p>
+            <p className="text-base text-left text-white">
+              {question.question}
+            </p>
             {question.choices.map((choice, choiceIndex) => (
               <div
                 key={choiceIndex}
-                className="my-4 flex text-left text-base text-white"
+                className="my-4 flex flex-row text-left text-base text-white"
               >
                 {
                   //Create multiple box choice
@@ -68,6 +105,7 @@ function PowerQuiz() {
                   checked={selectedChoices[index] === choiceIndex}
                   onChange={handleSelectChoice}
                   className="mr-2"
+                  disabled={index !== activeQuestionIndex}
                 />
                 <label
                   htmlFor={`question-${index}-choice-${choiceIndex}`}
@@ -79,7 +117,8 @@ function PowerQuiz() {
             ))}
           </div>
         ))}
-      <div>
+      <div className="md:container "></div>
+      <div className="md:container py-6">
         <button
           onClick={handleSubmit}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
